752a5a5dbc3bafe39dc2f4b1506138fdf8d14ef0
Mahalaxmi-Nadar <mahalaxminadar204@gmail.com>
2025-07-20 01:00:07 +0530

    Fixed port for Railway deployment

--- main.py
+++ main.py
@@ -195,5 +195,5 @@ def trending_api():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
